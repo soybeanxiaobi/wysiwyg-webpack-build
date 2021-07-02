87651a090cb336cad9eca9a7cb361139619b2ffc
chenxiaobin <chenxiaobin@youzan.com>
2021-07-02 18:32:01 +0800

    feat: white-case;

--- src/white-case/index.vue
+++ src/white-case/index.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
     <div :style="{ height: height }" />
-    <p>xiaobe-test-page-design-build</p>
   </div>
 </template>
 
 <script>
-import { White } from "@youzan/captain-showcase";
-
 export default {
-  name: "page-design-white",
+  name: "white-case",
 
   components: {
     [White.name]: White,
