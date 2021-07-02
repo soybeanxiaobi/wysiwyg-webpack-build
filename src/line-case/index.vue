f42add82fcb600dc11c36ba1909d25c0f350663f
chenxiaobin <chenxiaobin@youzan.com>
2021-07-02 18:42:47 +0800

    feat: 新增划线组件;

---
+++ src/line-case/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div className="line-case">
+    <div :style="createStyle(value)" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "line-case",
+
+  components: {
+    [White.name]: White,
+  },
+
+  props: {
+    color: String,
+    lineType: String,
+    hasPadding: Boolean,
+  },
+
+  methods: {
+    createStyle(value) {
+      const { color, hasPadding, lineType } = value;
+
+      return {
+        height: 0,
+        borderTopWidth: "1px",
+        margin: hasPadding ? "0 10px" : 0,
+        borderColor: color,
+        borderStyle: lineType,
+      };
+    },
+  },
+};
+</script>
+<style scoped>
+@import "./index.scss";
+</style>
